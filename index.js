0db14ec5365f6b885376ccaac858dcc38e87fcec
yozba <45725762+yozba@users.noreply.github.com>
2019-01-25 03:03:48 +0900

    masonryの追加など
    masonryの追加
    imagesLoadedの追加
    グリッドレイアウトの実装
    article.htmlファイルの若干の変更（画像を変更）

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-$(function(){
-    $("#content").load("article.html");
+$(function () {
+    $('#content').load('article.html', function (response, status, xhr) {
+        var $grid = $('#content').imagesLoaded(function(){
+            $grid.masonry({
+                itemSelector: '.card'
+            });
+        });     
+    });
 });
