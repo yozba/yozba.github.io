799606a26102630aee333d6399d50a17a5131827
yozba <zitome207@gmail.com>
2019-09-04 06:42:03 +0900

    html初心者
    わたしは　いんでっくすも　ふぁいるわけ　してしまいました

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,5 @@
 $(function () {
-    $('#content').load('article.html', function (response, status, xhr) {
+    $('#content').load('html/article.html', function (response, status, xhr) {
         var $grid = $('#content').imagesLoaded(function(){
             $grid.masonry({
                 itemSelector: '.card'
