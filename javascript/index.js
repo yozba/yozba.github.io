bf0cd7fd8a58edf404410e0b3c1f7322f1035763
yozba <zitome207@gmail.com>
2019-09-21 05:11:42 +0900

    他のページの仮実装など
    ・faviconの追加
    ・about/output on twitter/worksページの追加
    ・それに伴いarticle.html→output~.htmlに変更
    ・works.html/about.htmlの追加
    ・ボタンを押すと#contentの中身が変わるように(js)
    ・class textmanの追加とそのcss（仮）

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,27 @@
 $(function () {
-    $('#content').load('html/article.html', function (response, status, xhr) {
-        var $grid = $('#content').imagesLoaded(function(){
+    $('#content').load('html/works.html', function () {
+        var $grid = $('#loaded-content').imagesLoaded(function () {
             $grid.masonry({
                 itemSelector: '.card'
             });
         });
     });
 });
+
+function button_works(){
+    $('#content').load('html/works.html')
+}
+
+function button_output() {
+    $('#content').load('html/output_on_twitter.html', function () {
+        var $grid = $('#loaded-content').imagesLoaded(function () {
+            $grid.masonry({
+                itemSelector: '.card'
+            });
+        });
+    });
+}
+
+function button_about() {
+    $('#content').load('html/about.html');
+}
